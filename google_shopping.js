78f5737303b20e3e1ba83921e5d0a7f8952ccf9a
rasbabbo <rasbabbo@yahoo.com>
2014-07-08 20:39:56 -0700

    goog2

--- google_shopping.js
+++ google_shopping.js
@@ -1,20 +1,34 @@
-var data = require("./products.json") 
-var shopping = [];
-var prodNum = 0
-//console.log(data);
-// Write your solutions below
-for (i = 0; i < data["items"].length; i++) {
-	var currentItem = data.items[i].kind;
-	console.log(currentItem);
-	var prodNum = prodNum
-	 + 1;
+// var data = require("./products.json") 
+// var shopping = [];
+// var prodNum = 0
+// //console.log(data);
+// // Write your solutions below
+// for (i = 0; i < data["items"].length; i++) {
+// 	var currentItem = data.items[i].kind;
+// 	console.log(currentItem);
+// 	var prodNum = prodNum
+// 	 + 1;
 
-	// if (currentItem.kind === 'shopping#product') {
-	// shopping.push
-	// }
-}
-console.log(prodNum);
+// 	// if (currentItem.kind === 'shopping#product') {
+// 	// shopping.push
+// 	// }
+// }
+// console.log(prodNum);
 
 
 //
 // console.log(data["items"]);
+
+//QUESTION 2
+
+var data = require("./products.json");
+var items = data["items"];
+var backorders = [];
+
+for (i = 0; i < items.length; i++) {
+	if (items[i]['product']['inventories'][0]['availability'] === "backorder") {
+		console.log("backordered");
+		console.log(items[i]["product"]["title"]);
+	}
+}
+
