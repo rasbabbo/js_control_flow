c400552ecc9c9e119a24c207a08867dd79cd1384
rasbabbo <rasbabbo@yahoo.com>
2014-07-08 20:15:44 -0700

    goog1

--- google_shopping.js
+++ google_shopping.js
@@ -1,5 +1,20 @@
-var data = require("./products.json")
-
+var data = require("./products.json") 
+var shopping = [];
+var prodNum = 0
+//console.log(data);
 // Write your solutions below
+for (i = 0; i < data["items"].length; i++) {
+	var currentItem = data.items[i].kind;
+	console.log(currentItem);
+	var prodNum = prodNum
+	 + 1;
 
-console.log(data["items"]);
+	// if (currentItem.kind === 'shopping#product') {
+	// shopping.push
+	// }
+}
+console.log(prodNum);
+
+
+//
+// console.log(data["items"]);
